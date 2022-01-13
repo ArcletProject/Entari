cffb0603c4ad667b6babc28c80d4ec9fa89bb788
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2022-01-13 23:02:52 +0800

    0.0.2

--- edoves/builtin/mah/protocol.py
+++ edoves/builtin/mah/protocol.py
@@ -1,9 +1,14 @@
 from typing import Dict
 from edoves.data_source_info import DataSourceInfo
+from edoves.main.medium import BaseMedium
 from edoves.main.protocol import NetworkProtocol
+from edoves.main.typings import TData
 
 
 class MAHProtocol(NetworkProtocol):
+    async def parse_raw_data(self, data: TData) -> BaseMedium:
+        pass
+
     source_information = DataSourceInfo(
         platform="Tencent",
         name="mirai-api-http",
