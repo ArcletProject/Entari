6a4d1be7f9c2edaae62aae253c62ff02f84c0abd
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2022-01-13 23:00:12 +0800

    0.0.2

--- edoves/main/protocol.py
+++ edoves/main/protocol.py
@@ -1,25 +1,28 @@
-from abc import ABCMeta
-from typing import TYPE_CHECKING, Generic, Optional, Union
+from abc import ABCMeta, abstractmethod
+from typing import TYPE_CHECKING, Generic, Optional, Union, Type, TypeVar
 from .medium import BaseMedium
 from ..security import UNDEFINED, generate_identifier
 from ..data_source_info import DataSourceInfo
 from ..utilles import ModuleStatus
 from .exceptions import ValidationFailed, DataMissing
 from .typings import TConfig, TData
+from .controller import Controller
+
+TM = TypeVar("TM")
 
 
 if TYPE_CHECKING:
     from . import Edoves
-    from .module import BaseModule
+    from .module import BaseModule, MediumModule
 
 
 class AbstractProtocol(metaclass=ABCMeta):
-    client: "Edoves"
+    edoves: "Edoves"
     medium: TData
     __identifier: int = UNDEFINED
 
-    def __init__(self, client: "Edoves", identifier: Optional[Union[str, int]] = None):
-        self.client = client
+    def __init__(self, edoves: "Edoves", identifier: Optional[Union[str, int]] = None):
+        self.edoves = edoves
         if not identifier and self.__identifier == UNDEFINED:
             raise ValidationFailed
         if isinstance(identifier, int):
@@ -32,11 +35,16 @@ class AbstractProtocol(metaclass=ABCMeta):
         return self.__identifier
 
     def verify(self, other: Union[int, "BaseModule"]):
-        if isinstance(other, int) and other != self.client.identifier:
+        if isinstance(other, int) and other != self.edoves.identifier:
             raise ValidationFailed
-        if other.identifier != self.client.identifier:
+        if other.identifier != self.edoves.identifier:
             other.state = ModuleStatus.CLOSED
             raise ValidationFailed
+
+    def get_medium(self, medium_type: Optional[Type[TM]]) -> Union[TM, TData]:
+        if medium_type and not isinstance(self.medium, medium_type):
+            return medium_type.create(self.edoves.self, Type[self.medium])(self.medium)
+        return self.medium
 
     def __repr__(self):
         return (
@@ -45,7 +53,16 @@ class AbstractProtocol(metaclass=ABCMeta):
 
 
 class ModuleProtocol(AbstractProtocol):
+    __controller: Controller[Type["MediumModule"], "MediumModule"]
     medium: BaseMedium
+
+    def __init__(self, edoves: "Edoves", identifier: int):
+        super().__init__(edoves, identifier)
+        self.__controller = Controller(self.edoves)
+
+    @property
+    def modules(self) -> Controller[Type["MediumModule"], "MediumModule"]:
+        return self.__controller
 
 
 class NetworkProtocol(Generic[TConfig], AbstractProtocol):
@@ -57,6 +74,11 @@ class NetworkProtocol(Generic[TConfig], AbstractProtocol):
             raise DataMissing
         return super(NetworkProtocol, cls).__new__(cls)
 
-    def __init__(self, client: "Edoves", config: TConfig):
+    def __init__(self, edoves: "Edoves", config: TConfig):
         self.config = config
-        super().__init__(client, self.source_information.instance_identifier)
+        super().__init__(edoves, self.source_information.instance_identifier)
+
+    @abstractmethod
+    async def parse_raw_data(self, data: TData) -> BaseMedium:
+        """将server端传入的原始数据封装"""
+        raise NotImplementedError
