89574e628bd07764803f12a81a13a1624c2e4136
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2022-01-13 23:01:55 +0800

    0.0.2

--- edoves/builtin/mah/module/message.py
+++ edoves/builtin/mah/module/message.py
@@ -6,18 +6,26 @@ from edoves.main.typings import TMProtocol
 from ..types import MType
 
 
+async def test1(msg: Message):
+    return msg.content
+
+
+async def test2(msg: Message):
+    return msg.type
+
+
 class MessageModule(MediumModule):
     medium_type = Type[Message]
     identifier = VERIFY_CODE
 
     def __init__(self, protocol: TMProtocol):
         super().__init__(protocol)
+        self.new_handler(MType.ALL, test1, test2)
 
-        @self.new_handler(MType.ALL)
-        async def test1(msg: Message):
-            return msg.content
+    def test3(self):
+        self.protocol.modules.get("test")
 
 
-@MessageModule.prefab_handler(MType.Friend)
+@MessageModule.new_handler(MType.Friend)
 def test(msg: Message):
     return msg.purveyor
