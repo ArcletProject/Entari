ca9c828f6aeb5b06de6d8e6bed6e039e61e2c7b2
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2022-01-13 23:03:35 +0800

    0.0.2

--- edoves/builtin/mah/dock_server.py
+++
@@ -1,6 +0,0 @@
-from edoves.main.dock_server import BaseDockServer
-from edoves.security import MIRAI_API_HTTP_DEFAULT
-
-
-class MAHDockServer(BaseDockServer):
-    identifier = MIRAI_API_HTTP_DEFAULT
