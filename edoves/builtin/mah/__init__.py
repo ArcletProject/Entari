647b2453f61c8b0efc9d3d9cfaa3499d1ebddff9
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2022-01-13 23:02:29 +0800

    0.0.2

--- edoves/builtin/mah/__init__.py
+++ edoves/builtin/mah/__init__.py
@@ -2,11 +2,11 @@ from typing import Type
 from edoves.builtin.mah.protocol import MAHProtocol
 from ...security import MIRAI_API_HTTP_DEFAULT
 from ...main.config import TemplateConfig
-from edoves.builtin.mah.dock_server import MAHDockServer
+from edoves.builtin.mah.server_docker import MAHServerDocker
 
 VERIFY_CODE = MIRAI_API_HTTP_DEFAULT
 
 
 class MAHConfig(TemplateConfig):
     protocol: Type[MAHProtocol] = MAHProtocol
-    dock_server: Type[MAHDockServer] = MAHDockServer
+    server_docker: Type[MAHServerDocker] = MAHServerDocker
