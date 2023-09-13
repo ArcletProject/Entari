5ddc429a94bff15409686b170e0b24dd99401deb
RF-Tar-Railt <3165388245@qq.com>
2023-09-14 02:08:51 +0800

    :fire: remove all
    Signed-off-by: RF-Tar-Railt <3165388245@qq.com>

---
+++ old/arclet/edoves/__init__.py
@@ -0,0 +1,5 @@
+from typing import TYPE_CHECKING
+if not TYPE_CHECKING:
+    import arclet.edoves.builtin.event
+
+edoves_version = (0, 0, 15)
---
+++ old/arclet/edoves/main/context.pyi
@@ -0,0 +1,25 @@
+from typing import TYPE_CHECKING, Dict
+from arclet.letoderea.utils import ContextModel
+
+
+if TYPE_CHECKING:
+    from . import Edoves
+    from .scene import EdovesScene
+    from .event import EdovesBasicEvent
+    from .interact.module import BaseModule
+    from .interact.monomer import Monomer
+
+edoves_instance: "ContextModel[Edoves]" = ContextModel("edoves")
+current_scene: "ContextModel[EdovesScene]" = ContextModel("current_scene")
+ctx_module: "ContextModel[BaseModule]" = ContextModel("module")
+ctx_monomer: "ContextModel[Monomer]" = ContextModel("purveyor")
+ctx_event: "ContextModel[EdovesBasicEvent]" = ContextModel("event")
+
+
+context_map: Dict[str, ContextModel] = {
+    "Edoves": edoves_instance,
+    "Scene": current_scene,
+    "Event": ctx_event,
+    "Module": ctx_module,
+    "Monomer": ctx_monomer,
+}
