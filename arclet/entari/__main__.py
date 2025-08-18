3923dae08cb70bf91ff3e0f2e84e6ffd541d404f
RF-Tar-Railt <rf_tar_railt@qq.com>
2025-08-18 12:39:06 +0800

    :sparkles: enhance `entari new`

--- arclet/entari/__main__.py
+++ arclet/entari/__main__.py
@@ -2,21 +2,25 @@ import asyncio
 from datetime import datetime
 from importlib.util import find_spec
 from pathlib import Path
+import re
+import shutil
 from signal import SIGINT, SIGTERM, raise_signal
+import subprocess
+import sys
 from typing import Union
 
 from arclet.alconna import Alconna, Args, CommandMeta, MultiVar, Option, Subcommand, command_manager
 from arclet.alconna.exceptions import SpecialOptionTriggered
 from arclet.alconna.tools.formatter import RichConsoleFormatter
 from arclet.letoderea import publish
-from colorama.ansi import Fore
+from colorama.ansi import Fore, Style
 from satori import Api
 from satori.client import Account, ApiInfo
 from satori.event import Event
 from satori.exception import ActionFailed
 from satori.model import Channel, ChannelType, Guild, Login, LoginStatus, MessageObject, User
 
-from arclet.entari import Entari, listen
+from arclet.entari import Entari, __version__, listen
 from arclet.entari.config import EntariConfig
 from arclet.entari.event.base import MessageCreatedEvent
 from arclet.entari.event.lifespan import Ready
@@ -60,6 +64,7 @@ alc = Alconna(
     Subcommand("run", help_text="运行 Entari"),
     Subcommand("gen_main", help_text="生成一个 Entari 主程序文件"),
     Option("-c|--config", Args["path/", str], help_text="指定配置文件路径", dest="cfg_path"),
+    Option("-V|--version", help_text="查看版本信息"),
     meta=CommandMeta(
         "Entari App Launcher",
     ),
@@ -183,15 +188,46 @@ plugins:
 PLUGIN_DEFAULT_TEMPLATE = """\
 from arclet.entari import metadata
 
-metadata(name="{name}")
+metadata(
+    name="{name}",
+    author={author},
+    version="{version}",
+    description="{description}",
+)
 """
 
 
 PLUGIN_STATIC_TEMPLATE = """\
 from arclet.entari import declare_static, metadata
 
-metadata(name="{name}")
+metadata(
+    name="{name}",
+    author={author},
+    version="{version}",
+    description="{description}",
+)
 declare_static()
+"""
+
+PLUGIN_PROJECT_TEMPLATE = """\
+[project]
+name = "{name}"
+version = "{version}"
+description = "{description}"
+author = [
+    {author}
+]
+dependencies = [
+    "arclet.entari >= {entari_version}",
+]
+requires-python = {python_requirement}
+readme = "README.md"
+license = {license}
+"""
+
+README_TEMPLATE = """\
+# {name}
+{description}
 """
 
 MAIN_SCRIPT = """\
@@ -200,6 +236,43 @@ from arclet.entari import Entari
 app = Entari.load({path})
 app.run()
 """
+
+PYTHON_VERSION = sys.version_info[:2]
+
+
+def get_user_email_from_git() -> tuple[str, str]:
+    """Get username and email from git config.
+    Return empty if not configured or git is not found.
+    """
+    git = shutil.which("git")
+    if not git:
+        return "", ""
+    try:
+        username = subprocess.check_output([git, "config", "user.name"], text=True, encoding="utf-8").strip()
+    except subprocess.CalledProcessError:
+        username = ""
+    try:
+        email = subprocess.check_output([git, "config", "user.email"], text=True, encoding="utf-8").strip()
+    except subprocess.CalledProcessError:
+        email = ""
+    return username, email
+
+
+def validate_project_name(name: str) -> bool:
+    """Check if the project name is valid or not"""
+
+    pattern = r"^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$"
+    return re.fullmatch(pattern, name, flags=re.IGNORECASE) is not None
+
+
+def sanitize_project_name(name: str) -> str:
+    """Sanitize the project name and remove all illegal characters"""
+    pattern = r"[^a-zA-Z0-9\-_\.]+"
+    result = re.sub(pattern, "-", name)
+    result = re.sub(r"^[\._-]|[\._-]$", "", result)
+    if not result:
+        raise ValueError(f"Invalid project name: {name}")
+    return result
 
 
 def check_env(file: Path):
@@ -224,6 +297,9 @@ def main():
             print(alc.get_help())
         elif not isinstance(res.error_info, SpecialOptionTriggered):
             print(res.error_info)
+        return
+    if res.find("version"):
+        print(f"Entari {Fore.CYAN}{Style.BRIGHT}{__version__}{Fore.RESET}")
         return
     if res.find("config"):
         if res.find("config.new"):
@@ -272,46 +348,106 @@ def main():
             print(f"Current config file:\n{Fore.BLUE}{cfg.path.resolve()!s}")
             return
     if res.find("new"):
+        is_application = res.find("new.application")
+        if not is_application:
+            ans = input(f"Is this an plugin project? {Fore.MAGENTA}(Y/n): {Fore.RESET}").strip().lower()
+            is_application = ans in {"no", "false", "f", "0", "n", "n/a", "none", "nope", "nah"}
         name = res.query[str]("new.name")
         if not name:
-            print(f"{Fore.BLUE}Please specify a plugin name:")
-            name = input(f"{Fore.RESET}>>> ").strip()
-        is_application = res.find("new.application")
-        if not name.startswith("entari_plugin_") and not is_application:
-            print(f"{Fore.RED}Plugin will be corrected to 'entari_plugin_{name}' automatically.")
-            print(f"{Fore.RESET}If you want to keep the name, please use option {Fore.MAGENTA}-A|--application.")
-            name = f"entari_plugin_{name}"
+            name = input(f"Plugin name: {Fore.RESET}").strip()
+        if not validate_project_name(name):
+            print(f"{Fore.RED}Invalid plugin name: {name!r} {Fore.RESET}")
+            return
+        proj_name = sanitize_project_name(name).replace(".", "-").replace("_", "-")
+        if not proj_name.lower().startswith("entari-plugin-") and not is_application:
+            print(f"{Fore.RED}Plugin will be corrected to 'entari-plugin-{proj_name}' automatically.")
+            print(
+                f"{Fore.YELLOW}If you want to keep the name, please use option {Fore.MAGENTA}-A|--application.{Fore.RESET}"  # noqa: E501
+            )
+            proj_name = f"entari-plugin-{proj_name}"
+        file_name = proj_name.replace("-", "_")
+        version = input(f"Plugin version {Fore.MAGENTA}(0.1.0){Fore.RESET}: ").strip() or "0.1.0"
+        description = input(f"Plugin description: {Fore.RESET}").strip()
+        git_user, git_email = get_user_email_from_git()
+        author = input(f"Plugin author name {Fore.MAGENTA}({git_user}){Fore.RESET}: ").strip() or git_user
+        email = input(f"Plugin author email {Fore.MAGENTA}({git_email}){Fore.RESET}: ").strip() or git_email
+        if not is_application:
+            default_python_requires = f">={PYTHON_VERSION[0]}.{PYTHON_VERSION[1]}"
+            python_requires = (
+                input(
+                    f"Python requires('*' to allow any) {Fore.MAGENTA}({default_python_requires}){Fore.RESET}: "
+                ).strip()
+                or default_python_requires
+            )
+            licence = input(f"License(SPDX name) {Fore.MAGENTA}(MIT){Fore.RESET}: ").strip() or "MIT"
+        else:
+            python_requires = ""
+            licence = ""
         is_file = res.find("new.file")
+        if not is_file:
+            ans = input(f"Is this a single file plugin? {Fore.MAGENTA}(Y/n): {Fore.RESET}").strip().lower()
+            is_file = ans in {"yes", "true", "t", "1", "y", "yea", "yeah", "yep", "sure", "ok", "okay", ""}
         is_static = res.find("new.static")
-        if name.startswith("entari_plugin_") and find_spec(name):
-            print(f"'{name}' already installed, please use another name.")
+        if not is_static:
+            ans = input(f"Is this a disposable plugin? {Fore.MAGENTA}(Y/n): {Fore.RESET}").strip().lower()
+            is_static = ans in {"no", "false", "f", "0", "n", "n/a", "none", "nope", "nah"}
+        if proj_name.startswith("entari-plugin-") and find_spec(file_name):
+            print(f"{Fore.RED}'{proj_name}' already installed, please use another name.{Fore.RESET}")
             return
         path = Path.cwd() / ("plugins" if is_application else "src")
         path.mkdir(parents=True, exist_ok=True)
         if is_file:
-            path = path.joinpath(f"{name}.py")
+            path = path.joinpath(f"{file_name}.py")
         else:
-            path = path.joinpath(name, "__init__.py")
+            path = path.joinpath(file_name, "__init__.py")
             path.parent.mkdir(exist_ok=True)
         with path.open("w+", encoding="utf-8") as f:
-            f.write((PLUGIN_STATIC_TEMPLATE if is_static else PLUGIN_DEFAULT_TEMPLATE).format(name=name))
+            t = PLUGIN_STATIC_TEMPLATE if is_static else PLUGIN_DEFAULT_TEMPLATE
+            f.write(
+                t.format(
+                    name=proj_name,
+                    author=f'[{{"name": "{author}", "email": "{email}"}}]',
+                    version=version,
+                    description=description,
+                )
+            )
+        if not is_application:
+            toml_path = Path.cwd() / "pyproject.toml"
+            if not toml_path.exists():
+                with toml_path.open("w+", encoding="utf-8") as f:
+                    f.write(
+                        PLUGIN_PROJECT_TEMPLATE.format(
+                            name=proj_name,
+                            version=version,
+                            description=description,
+                            author=f'{{"name" = "{author}", "email" = "{email}"}}',
+                            entari_version=__version__,
+                            python_requirement=f'"{python_requires}"',
+                            license=f'{{"text" = "{licence}"}}',
+                        )
+                    )
+            readme_path = Path.cwd() / "README.md"
+            if not readme_path.exists():
+                with readme_path.open("w+", encoding="utf-8") as f:
+                    f.write(README_TEMPLATE.format(name=proj_name, description=description))
         cfg = EntariConfig.load(res.query[str]("cfg_path.path", None))
-        if name in cfg.plugin:
+        if file_name in cfg.plugin:
             return
-        if f"entari_plugin_{name}" in cfg.plugin:
+        if f"entari_plugin_{file_name}" in cfg.plugin:
             return
-        if name.removeprefix("entari_plugin_") in cfg.plugin:
+        if file_name.removeprefix("entari_plugin_") in cfg.plugin:
             return
-        cfg.plugin[name] = {}
+        cfg.plugin[file_name] = {}
         if res.find("new.disabled"):
-            cfg.plugin[name]["$disable"] = True
+            cfg.plugin[file_name]["$disable"] = True
         if res.find("new.optional"):
-            cfg.plugin[name]["$optional"] = True
+            cfg.plugin[file_name]["$optional"] = True
         if res.find("new.priority"):
-            cfg.plugin[name]["priority"] = res.query[int]("new.priority.num", 16)
+            cfg.plugin[file_name]["priority"] = res.query[int]("new.priority.num", 16)
         if is_application:
             cfg._origin_data["basic"].setdefault("external_dirs", []).append("plugins")
         cfg.save()
+        print(f"{Fore.GREEN}Plugin created at {path}.{Fore.RESET}")
         return
     if res.find("add"):
         name = res.query[str]("add.name")
