f65fd15e3d9e9e9c043e5cac51ae05f441c9f579
RF-Tar-Railt <rf_tar_railt@qq.com>
2026-03-27 20:51:02 +0800

    :bug: add catch ActionFailed in SendXX Event

--- arclet/entari/event/send.py
+++ arclet/entari/event/send.py
@@ -2,8 +2,10 @@ from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 from arclet.letoderea import Contexts, Result, define, provide
+from satori import ChannelType
 from satori.client import Account
-from satori.model import MessageObject
+from satori.exception import ActionFailed
+from satori.model import Channel, MessageObject
 
 from ..const import ITEM_ACCOUNT, ITEM_CHANNEL, ITEM_MESSAGE_CONTENT, ITEM_SESSION
 from ..message import MessageChain
@@ -30,10 +32,17 @@ before_send_pub = define(SendRequest, name="entari.event/before_send")
 @before_send_pub.gather
 async def req_gather(req: SendRequest, context: Contexts):
     context[ITEM_ACCOUNT] = req.account
-    context[ITEM_CHANNEL] = await req.account.channel_get(req.channel)
     context[ITEM_MESSAGE_CONTENT] = req.message
     if req.session:
         context[ITEM_SESSION] = req.session
+        context[ITEM_CHANNEL] = req.session.channel
+    else:
+        try:
+            context[ITEM_CHANNEL] = await req.account.channel_get(req.channel)
+        except ActionFailed:
+            context[ITEM_CHANNEL] = Channel(
+                req.channel, ChannelType.DIRECT if req.channel.startswith("private:") else ChannelType.TEXT
+            )
 
 
 @dataclass
@@ -52,8 +61,15 @@ send_pub.bind(provide(list[MessageObject], call="$resp_result"))
 @send_pub.gather
 async def resp_gather(resp: SendResponse, context: Contexts):
     context[ITEM_ACCOUNT] = resp.account
-    context[ITEM_CHANNEL] = await resp.account.channel_get(resp.channel)
     context[ITEM_MESSAGE_CONTENT] = resp.message
     context["$resp_result"] = resp.result
     if resp.session:
         context[ITEM_SESSION] = resp.session
+        context[ITEM_CHANNEL] = resp.session.channel
+    else:
+        try:
+            context[ITEM_CHANNEL] = await resp.account.channel_get(resp.channel)
+        except ActionFailed:
+            context[ITEM_CHANNEL] = Channel(
+                resp.channel, ChannelType.DIRECT if resp.channel.startswith("private:") else ChannelType.TEXT
+            )
